1e58edc44c4b641210dbb9912b64e717fe0ba7ea
Paul Kim <me@pkim.co>
2016-12-10 20:43:40 -0800

    Cleaned up initial GoogleMaps code

--- MeetupWithFriends/MapViewController.swift
+++ MeetupWithFriends/MapViewController.swift
@@ -8,41 +8,46 @@
 
 import UIKit
 import GoogleMaps
+import CoreLocation
 
 class MapViewController: UIViewController {
-    // Mark: - Properties
+    // Mark: Properties
+
+    @IBOutlet weak var googleMapView: GMSMapView!
+    var locationManager = CLLocationManager()
     
-    @IBOutlet weak var googleMapView: UIView!
-    var mapView: GMSMapView!
-    
+    // Mark: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create GMSCameraPosition that tells the map to display the
-        // user's current location at zoom level 6
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        mapView = GMSMapView.map(withFrame: googleMapView.bounds, camera: camera)
+        // set up Core Location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        // create GMSCamera and assign to map
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 15.0)
+        googleMapView.camera = camera
         
         // map configuration
-        mapView.isMyLocationEnabled = true
-        mapView.settings.compassButton = true
-        mapView.settings.zoomGestures = true
-        mapView.settings.myLocationButton = true
-        
-        // add to view
-        googleMapView.addSubview(mapView)
-        
-        // creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
+        googleMapView.settings.compassButton = true
+        googleMapView.settings.zoomGestures = true
+        googleMapView.settings.myLocationButton = true
     }
 }
 
-// Mark: - GoogleMaps Methods
-extension MapViewController {
+// Mark: - MapViewController: GMSMapViewDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            googleMapView.isMyLocationEnabled = true
+        } else {
+            // TODO: prompt for location
+        }
+    }
+}
 
+// Mark: - MapViewController: GMSMapViewDelegate
+extension MapViewController: GMSMapViewDelegate {
+    
 }
 
