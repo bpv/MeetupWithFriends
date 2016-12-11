582e3c43b2982270754496e5414b26d6fa009742
Paul Kim <me@pkim.co>
2016-12-10 19:13:45 -0800

    Initial GoogleMaps implementation.  Map is displayed and can get user location. Map size is off, will fix later.

--- MeetupWithFriends/MapViewController.swift
+++ MeetupWithFriends/MapViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
-
+    // Mark: - Properties
+    
+    @IBOutlet weak var googleMapView: UIView!
+    var mapView: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // create GMSCameraPosition that tells the map to display the
+        // user's current location at zoom level 6
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        mapView = GMSMapView.map(withFrame: googleMapView.bounds, camera: camera)
+        
+        // map configuration
+        mapView.isMyLocationEnabled = true
+        mapView.settings.compassButton = true
+        mapView.settings.zoomGestures = true
+        mapView.settings.myLocationButton = true
+        
+        // add to view
+        googleMapView.addSubview(mapView)
+        
+        // creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+// Mark: - GoogleMaps Methods
+extension MapViewController {
 
 }
 
