f4a1041ab82aa44dd245eb54b0d3adfd571a249c
Paul Kim <me@pkim.co>
2016-12-10 16:59:31 -0800

    GoogleConstants now uses settings from Settings.plist

--- MeetupWithFriends/GoogleConstants.swift
+++ MeetupWithFriends/GoogleConstants.swift
@@ -9,7 +9,24 @@
 import Foundation
 
 struct GoogleConstants {
+    static var settingsDict: [String: AnyObject] {
+        var format = PropertyListSerialization.PropertyListFormat.xml //format of the property list
+        var plistData:[String:AnyObject] = [:]  //our data
+        let plistPath:String? = Bundle.main.path(forResource: "Settings", ofType: "plist")! //the path of the data
+        let plistXML = FileManager.default.contents(atPath: plistPath!)! //the data in XML format
+        do { //convert the data to a dictionary and handle errors.
+            plistData = try PropertyListSerialization.propertyList(from: plistXML, options: .mutableContainersAndLeaves, format: &format) as! [String : AnyObject]
+            
+            return plistData
+        }
+        catch { // error condition
+            print("Error reading plist: \(error), format: \(format)")
+            return [String: AnyObject]()
+        }
+    }
+    
     struct API {
-        static var APIKey = "API Key Here"
+        static var ProjectID = GoogleConstants.settingsDict["googleProjectID"] as! String
+        static var APIKey = GoogleConstants.settingsDict["googleMapsAPIKey"] as! String
     }
 }
