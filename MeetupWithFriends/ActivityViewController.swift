bd1af9365025e162bdd835d6e1455fbc4f491249
Paul Kim <me@pkim.co>
2016-12-31 13:51:45 -0800

    Initial integration of FireBaseAuthUi with Google Sign In

--- MeetupWithFriends/ActivityViewController.swift
+++ MeetupWithFriends/ActivityViewController.swift
@@ -7,17 +7,47 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuthUI
+import FirebaseGoogleAuthUI
+import FirebaseFacebookAuthUI
 
 class ActivityViewController: UIViewController {
     
     // Mark: Properties
+    
+    var user: FIRUser?
+    fileprivate var _authHandle: FIRAuthStateDidChangeListenerHandle!
     
     let activities = ["restaurant", "bar", "night_club", "cafe", "movie_theater"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureAuth()
+    }
+    
+    // Mark: Config
+    
+    func configureAuth() {
+        let provider: [FUIAuthProvider] = [FUIGoogleAuth()]
+        FUIAuth.defaultAuthUI()?.providers = provider
+        
+        // listen for changes in the authorization state
+        _authHandle = FIRAuth.auth()?.addStateDidChangeListener { (auth: FIRAuth, user: FIRUser?) in
+            // TODO: refresh data if necessary
+            
+            // check if there is a current user
+            if let activeUser = user {
+                // check if the current app user is the current FIRUser
+                if self.user != activeUser {
+                    self.user = activeUser
+                }
+            } else {
+                // user must sign in
+                Helpers.loginSession(view: self)
+            }
+        }
     }
     
     // Mark: Actions
--- MeetupWithFriends/Helpers.swift
+++ MeetupWithFriends/Helpers.swift
@@ -8,13 +8,22 @@
 
 import Foundation
 import UIKit
+import FirebaseAuthUI
+
+// Mark: - Helpers
 
 class Helpers {
+    
     class func displayError(view: UIViewController!, errorString: String!) {
         if let errorString = errorString {
             let alert = UIAlertController(title: "Error", message: errorString, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
             view.present(alert, animated: true, completion: nil)
         }
+    }
+    
+    class func loginSession(view: UIViewController!) {
+        let authViewController = FUIAuth.defaultAuthUI()!.authViewController()
+        view.present(authViewController, animated: true, completion: nil)
     }
 }
