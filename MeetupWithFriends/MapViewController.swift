984747d37ba8bbf1efe617f936e77ec823d336ad
Paul Kim <me@pkim.co>
2016-12-31 13:52:24 -0800

    Re-org/cleanup of code of MapViewController

--- MeetupWithFriends/MapViewController.swift
+++ MeetupWithFriends/MapViewController.swift
@@ -8,30 +8,35 @@
 
 import UIKit
 import GoogleMaps
-import GooglePlaces
 import CoreLocation
 
-class MapViewController: UIViewController {
-    // Mark: Properties
+// Mark: - MapViewController
 
-    @IBOutlet weak var googleMapView: GMSMapView!
+class MapViewController: UIViewController {
+    
+    // Mark: Properties
+    
     var locationManager = CLLocationManager()
-    var placesClient: GMSPlacesClient!
     var places: Places!
     var type: String!
     
-    // Mark: View Lifecycle
+    // Mark: Outlets
+    
+    @IBOutlet weak var googleMapView: GMSMapView!
+    
+    // Mark: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // set up placesClient
-        placesClient = GMSPlacesClient.shared()
         
         // set up Core Location
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         
+        configureMap()
+    }
+    
+    func configureMap() {
         // create GMSCamera and assign to map
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: GoogleConstants.Configuration.startingZoom)
         googleMapView.camera = camera
@@ -42,6 +47,53 @@ class MapViewController: UIViewController {
         googleMapView.settings.myLocationButton = true
     }
     
+    // Mark: Load Nearby Places
+    
+    func loadNearbyPlaces(latitude: Double, longitude: Double, pageToken: String?) {
+        // load resuts from Places API
+        GooglePlacesConvenience.getNearbyPlaces(latitude: latitude, longitude: longitude, type: type, pageToken: nil, withCompletionHandler: { (places, error) in
+            
+            guard error == nil else {
+                Helpers.displayError(view: self, errorString: error)
+                return
+            }
+            
+            self.places = places
+
+            self.createMarkers()
+            
+            self.displayCards()
+        })
+    }
+    
+    func createMarkers() {
+        performUIUpdatesOnMain {
+            for place in self.places.places {
+                let position = CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)
+                let marker = GMSMarker(position: position)
+                marker.title = place.name
+                
+                // get the image
+                // TODO: cache icon images
+                var data: Data
+                do {
+                    data = try Data(contentsOf: URL(string: place.icon)!)
+                } catch {
+                    continue
+                }
+                marker.icon = UIImage(data: data, scale: 3)
+                marker.map = self.googleMapView
+            }
+        }
+    }
+    
+    // Mark: Center Map
+    
+    func centerMap(latitude: Double, longitude: Double) {
+        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: GoogleConstants.Configuration.startingZoom)
+        googleMapView.animate(to: camera)
+    }
+
     // Mark: Actions
     
     @IBAction func findAddress(_ sender: Any) {
@@ -91,61 +143,18 @@ class MapViewController: UIViewController {
         present(addressAlert, animated: true, completion: nil)
     }
     
-    func loadNearbyPlaces(latitude: Double, longitude: Double, pageToken: String?) {
-        // load resuts from Places API
-        GooglePlacesConvenience.getNearbyPlaces(latitude: latitude, longitude: longitude, type: type, pageToken: nil, withCompletionHandler: { (places, error) in
-            
-            guard error == nil else {
-                Helpers.displayError(view: self, errorString: error)
-                return
-            }
-            
-            self.places = places
-
-            self.createMarkers()
-            
-            self.displayCards()
-        })
-    }
-    
-    func createMarkers() {
-        performUIUpdatesOnMain {
-            for place in self.places.places {
-                let position = CLLocationCoordinate2D(latitude: place.latitude, longitude: place.longitude)
-                let marker = GMSMarker(position: position)
-                marker.title = place.name
-                
-                // get the image
-                // TODO: cache icon images
-                var data: Data
-                do {
-                    data = try Data(contentsOf: URL(string: place.icon)!)
-                } catch {
-                    continue
-                }
-                marker.icon = UIImage(data: data, scale: 3)
-                marker.map = self.googleMapView
-            }
-        }
-    }
-    
-    // center the map on coordinates
-    func centerMap(latitude: Double, longitude: Double) {
-        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: longitude, zoom: GoogleConstants.Configuration.startingZoom)
-        googleMapView.animate(to: camera)
-    }
-    
     @IBAction func displayCards() {
         let controller = storyboard?.instantiateViewController(withIdentifier: "PlaceCardViewController") as! PlaceCardViewController
         
         // load places
         controller.places = places
-            
+        
         present(controller, animated: true, completion: nil)
     }
 }
 
-// Mark: - GMSMapViewDelegate
+// Mark: - MapViewController: GMSMapViewDelegate
+
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedWhenInUse {
@@ -156,7 +165,8 @@ extension MapViewController: CLLocationManagerDelegate {
     }
 }
 
-// Mark: - GMSMapViewDelegate
+// Mark: - MapViewController: GMSMapViewDelegate
+
 extension MapViewController: GMSMapViewDelegate {
     
 }
